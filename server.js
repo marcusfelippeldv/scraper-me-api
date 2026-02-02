5ddafc271e45a87d82897aae189475674af4b0a1
marcusfelippeldv <marcusfelippeldv@gmail.com>
2026-02-02 15:28:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const express = require('express');
 const { chromium } = require('playwright');
 
 const app = express();
+
+// CORS - Permitir requisições do Lovable
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Scraper-Token');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
